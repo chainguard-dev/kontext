17ea4dee5ba78363c09f0d0be743e7fe4cc2d2f7
Matt Moore <mattmoor@chainguard.dev>
2023-05-16 07:56:03 -0700

    Create an initial fork of the core library code (#2)

---
+++ bundle_test.go
@@ -0,0 +1,25 @@
+/*
+Copyright 2023 Chainguard, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package kontext
+
+import (
+	"testing"
+)
+
+func TestBundleLayerIndex(t *testing.T) {
+	// Check that if we bundle testdata it has the expected size.
+	l, err := bundle("./testdata")
+	if err != nil {
+		t.Error("bundle() =", err)
+	}
+	sz, err := l.Size()
+	if err != nil {
+		t.Error("l.Size() =", err)
+	}
+	if got, want := sz, int64(204); got != want {
+		t.Errorf("Size() = %d, wanted %d", got, want)
+	}
+}
---
+++ expand_test.go
@@ -0,0 +1,59 @@
+/*
+Copyright 2023 Chainguard, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package kontext
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestExpand(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal("os.Getwd() =", err)
+	}
+
+	// "expand" testdata into a new temporary directory.
+	src := filepath.Join(wd, "testdata")
+	dest, err := os.MkdirTemp("", "")
+	if err != nil {
+		t.Fatal("os.MkdirTemp() =", err)
+	}
+	defer os.RemoveAll(dest)
+	if err := os.Chdir(dest); err != nil {
+		t.Fatal("os.Chdir() =", err)
+	}
+	if err := expand(context.Background(), src); err != nil {
+		t.Error("expand() =", err)
+	}
+
+	// bundle up both directories.
+	lSrc, err := bundle(src)
+	if err != nil {
+		t.Error("bundle() =", err)
+	}
+	lDest, err := bundle(dest)
+	if err != nil {
+		t.Error("bundle() =", err)
+	}
+
+	// Compute the bundle hashes
+	hSrc, err := lSrc.Digest()
+	if err != nil {
+		t.Error("lSrc.Digest() =", err)
+	}
+	hDest, err := lDest.Digest()
+	if err != nil {
+		t.Error("lDest.Digest() =", err)
+	}
+
+	// Make sure they match.
+	if hSrc != hDest {
+		t.Errorf("bundle() = %v, wanted %v", hDest, hSrc)
+	}
+}
