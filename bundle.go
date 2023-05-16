ce078e9d81f5132763f6772c95c989a7d4ce3b94
Matt Moore <mattmoor@chainguard.dev>
2023-05-16 07:39:58 -0700

    Create an initial fork of the core library code

---
+++ bundle.go
@@ -0,0 +1,109 @@
+/*
+Copyright 2023 Chainguard, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package kontext
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/google/go-containerregistry/pkg/name"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
+)
+
+var (
+	// BaseImageString holds a reference to a built image of ./cmd/kontext-expander
+	// See ./hack/build-flags.sh for how this is replaced at link-time.
+	BaseImageString = "ghcr.io/mattmoor/github.com/mattmoor/mink/cmd/kontext-expander:latest"
+	// BaseImage is where we publish ./cmd/kontext-expander
+	BaseImage, _ = name.ParseReference(BaseImageString)
+)
+
+func bundle(directory string) (v1.Layer, error) {
+	buf := bytes.NewBuffer(nil)
+	tw := tar.NewWriter(buf)
+	defer tw.Close()
+
+	err := filepath.Walk(directory,
+		func(path string, fi os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			// Skip anything in the .git directory
+			// TODO(mattmoor): expand this to .gitignore / .dockerignore?
+			if fi.IsDir() && filepath.Base(path) == ".git" {
+				return filepath.SkipDir
+			}
+
+			// Chase symlinks.
+			info, err := os.Stat(path)
+			if err != nil {
+				return err
+			}
+
+			// Compute the path relative to the base path
+			relativePath, err := filepath.Rel(directory, path)
+			if err != nil {
+				return err
+			}
+
+			newPath := filepath.Join(StoragePath, relativePath)
+
+			if info.Mode().IsDir() {
+				return tw.WriteHeader(&tar.Header{
+					Name:     newPath,
+					Typeflag: tar.TypeDir,
+					Mode:     0555,
+				})
+			}
+
+			// Open the file to copy it into the tarball.
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+
+			// Copy the file into the image tarball.
+			if err := tw.WriteHeader(&tar.Header{
+				Name:     newPath,
+				Size:     info.Size(),
+				Typeflag: tar.TypeReg,
+				// Use a fixed Mode, so that this isn't sensitive to the directory and umask
+				// under which it was created. Additionally, windows can only set 0222,
+				// 0444, or 0666, none of which are executable.
+				Mode: 0555,
+			}); err != nil {
+				return err
+			}
+			_, err = io.Copy(tw, file)
+			return err
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	return tarball.LayerFromReader(bytes.NewBuffer(buf.Bytes()))
+}
+
+// Bundle packages up the given directory as a self-extracting container image based
+// on BaseImage and publishes it to tag.
+func Bundle(ctx context.Context, directory string, tag name.Tag) (name.Digest, error) {
+	layer, err := bundle(directory)
+	if err != nil {
+		return name.Digest{}, err
+	}
+
+	return Map(ctx, BaseImage, tag, func(ctx context.Context, img v1.Image) (v1.Image, error) {
+		return mutate.AppendLayers(img, layer)
+	})
+}
---
+++ expand.go
@@ -0,0 +1,95 @@
+/*
+Copyright 2023 Chainguard, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package kontext
+
+import (
+	"context"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	// StoragePath is where in the container image the files are placed.
+	StoragePath = "/var/run/kontext"
+)
+
+func copy(src, dest string) error {
+	from, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer from.Close()
+
+	to, err := os.OpenFile(dest, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer to.Close()
+
+	_, err = io.Copy(to, from)
+	return err
+}
+
+func expand(ctx context.Context, base string) error {
+	targetPath, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.SetLimit(100)
+
+	if err := filepath.Walk(base, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if path == base {
+			return nil
+		}
+
+		// Add each file to the backlog.
+		eg.Go(func() error {
+			// If the context is canceled, then bail out early.
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			default:
+			}
+
+			relativePath := path[len(base)+1:]
+			target := filepath.Join(targetPath, relativePath)
+
+			if info.IsDir() {
+				return os.MkdirAll(target, os.ModePerm)
+			}
+			if !info.Mode().IsRegular() {
+				log.Printf("Skipping irregular file: %q", relativePath)
+				return nil
+			}
+			if err := os.MkdirAll(filepath.Dir(target), os.ModePerm); err != nil {
+				return err
+			}
+			return copy(path, target)
+		})
+
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	// Wait for the work to be done.
+	return eg.Wait()
+}
+
+// Expand recursively copies the current working directory into StoragePath.
+func Expand(ctx context.Context) error {
+	return expand(ctx, StoragePath)
+}
